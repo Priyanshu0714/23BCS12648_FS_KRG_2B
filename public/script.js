27cd6c67b6069a80e7bee53e4fa5d892fc0cd458
Priyanshu Choudhary <priyanshu@Priyanshus-MacBook-Air.local>
2025-02-25 11:29:01 +0530

    updated button in glide

--- public/script.js
+++ public/script.js
@@ -1,323 +1,172 @@
-function _0x7e19() {
-  const _0x3688a9 = [
-    "contactme",
-    "getElementById",
-    "100XHXXTu",
-    "click",
-    "/image.avif\x27;\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20",
-    "\x22\x20target=\x22_blank\x22>",
-    "</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<a\x20class=\x22text-lg\x22\x20href=\x22",
-    "hamburgerScroll",
-    "projects/",
-    "all",
-    "style",
-    "projectscroll",
-    "mount",
-    "opacity-100",
-    "blink\x200.5s\x20infinite",
-    "body",
-    "2261490PJXnIn",
-    "typewriter",
-    "createElement",
-    "glide__slide",
-    "animation",
-    ".glide-trending",
-    "remove",
-    "filter",
-    "replace",
-    "add",
-    "title",
-    "smooth",
-    "link",
-    "json",
-    "fixed",
-    ".glide__slide",
-    "75388sxFGFY",
-    "/image.png\x22\x20alt=\x22image\x22\x20onerror=\x22this.onerror=null;\x20this.src=\x27projects/",
-    "13513203EdKQJh",
-    "relative",
-    "Projects",
-    "</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22text-sm\x22>",
-    "translate-x-0",
-    "contactmescroll",
-    "target",
-    "translate-x-full",
-    "map",
-    "innerHTML",
-    "description",
-    "querySelector",
-    "opacity-0",
-    "closest",
-    "mouseout",
-    "mouseover",
-    "Hi,\x20I\x20am\x20a\x20Developer.",
-    "12CyHRbe",
-    "addEventListener",
-    "div",
-    "hidden",
-    "closebutton",
-    "2906696nwYgpX",
-    "3262830Gemger",
-    "appendChild",
-    "projectpart",
-    "44552zoeyyK",
-    "2295286uXLJFn",
-    "classList",
-    "about",
-    ".glide__slides",
-    "aboutmescroll",
-    "pointer-events-none",
-    "\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22hidden\x20absolute\x20bottom-0\x20h-auto\x20w-full\x20bg-white\x20opacity-80\x20rounded-lg\x20z-10\x20items-center\x20justify-center\x20flex-col\x20space-y-2\x20overflow-y-scroll\x20p-2\x20border-t-2\x20border-gray-400\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22text-2xl\x20font-bold\x22>",
-    "toggle",
-    "scrollIntoView",
-    "length",
-  ];
-  _0x7e19 = function () {
-    return _0x3688a9;
-  };
-  return _0x7e19();
-}
-const _0x4ed9dc = _0x3a55;
-(function (_0x319096, _0x4b096b) {
-  const _0x54ad2b = _0x3a55,
-    _0x1a7046 = _0x319096();
-  while (!![]) {
-    try {
-      const _0x26176 =
-        (-parseInt(_0x54ad2b(0x1d4)) / 0x1) *
-          (-parseInt(_0x54ad2b(0x208)) / 0x2) +
-        -parseInt(_0x54ad2b(0x1f8)) / 0x3 +
-        (-parseInt(_0x54ad2b(0x1dd)) / 0x4) *
-          (parseInt(_0x54ad2b(0x1ea)) / 0x5) +
-        -parseInt(_0x54ad2b(0x1da)) / 0x6 +
-        parseInt(_0x54ad2b(0x1de)) / 0x7 +
-        -parseInt(_0x54ad2b(0x1d9)) / 0x8 +
-        parseInt(_0x54ad2b(0x20a)) / 0x9;
-      if (_0x26176 === _0x4b096b) break;
-      else _0x1a7046["push"](_0x1a7046["shift"]());
-    } catch (_0x1ae8f5) {
-      _0x1a7046["push"](_0x1a7046["shift"]());
-    }
+const slidesContainer = document.querySelector(".glide__slides");
+
+slidesContainer.addEventListener("mouseover", (event) => {
+  const hoverDiv = event.target.closest(".glide__slide")?.querySelector("div");
+  if (hoverDiv) {
+    hoverDiv.classList.remove("hidden");
   }
-})(_0x7e19, 0x61289);
-function _0x3a55(_0x462988, _0x3d8820) {
-  const _0x7e19d9 = _0x7e19();
-  return (
-    (_0x3a55 = function (_0x3a5572, _0x1d63fb) {
-      _0x3a5572 = _0x3a5572 - 0x1cc;
-      let _0x453f63 = _0x7e19d9[_0x3a5572];
-      return _0x453f63;
-    }),
-    _0x3a55(_0x462988, _0x3d8820)
-  );
-}
-const slidesContainer = document[_0x4ed9dc(0x1ce)](_0x4ed9dc(0x1e1));
-slidesContainer["addEventListener"](_0x4ed9dc(0x1d2), (_0x5a06c2) => {
-  const _0x1645ce = _0x4ed9dc,
-    _0x320cf3 = _0x5a06c2[_0x1645ce(0x210)]
-      [_0x1645ce(0x1d0)](".glide__slide")
-      ?.[_0x1645ce(0x1ce)](_0x1645ce(0x1d6));
-  _0x320cf3 && _0x320cf3[_0x1645ce(0x1df)]["remove"](_0x1645ce(0x1d7));
-}),
-  slidesContainer[_0x4ed9dc(0x1d5)](_0x4ed9dc(0x1d1), (_0x492d1d) => {
-    const _0x521cab = _0x4ed9dc,
-      _0x548633 = _0x492d1d[_0x521cab(0x210)]
-        ["closest"](_0x521cab(0x207))
-        ?.[_0x521cab(0x1ce)](_0x521cab(0x1d6));
-    _0x548633 && _0x548633[_0x521cab(0x1df)]["add"](_0x521cab(0x1d7));
-  }),
-  slidesContainer["addEventListener"](_0x4ed9dc(0x1eb), (_0x4ae6c6) => {
-    const _0x3e1c53 = _0x4ed9dc,
-      _0x204986 = _0x4ae6c6["target"]
-        ["closest"](_0x3e1c53(0x207))
-        ?.["querySelector"](_0x3e1c53(0x1d6));
-    _0x204986 && _0x204986["classList"][_0x3e1c53(0x1e5)](_0x3e1c53(0x1d7));
-  });
+});
+
+slidesContainer.addEventListener("mouseout", (event) => {
+  const hoverDiv = event.target.closest(".glide__slide")?.querySelector("div");
+  if (hoverDiv) {
+    hoverDiv.classList.add("hidden");
+  }
+});
+
+slidesContainer.addEventListener("click", (event) => {
+  const hoverDiv = event.target.closest(".glide__slide")?.querySelector("div");
+  if (hoverDiv) {
+    hoverDiv.classList.toggle("hidden");
+  }
+});
+
 async function fetchprojectfolder() {
-  const _0x5c729e = _0x4ed9dc,
-    _0x178b7b = await fetch(_0x5c729e(0x1f0)),
-    _0x21bf19 = await _0x178b7b[_0x5c729e(0x205)](),
-    _0xf675c2 = document[_0x5c729e(0x1ce)](".glide__slides"),
-    _0x2446a2 = _0x21bf19[_0x5c729e(0x1ff)](
-      (_0x213a0c) => !_0x213a0c["includes"]("."),
-    )[_0x5c729e(0x212)](async (_0x306078) => {
-      const _0x2d948a = _0x5c729e,
-        _0x5cb544 = await fetch(_0x2d948a(0x1f0) + _0x306078 + "/info.json"),
-        _0x5277c1 = await _0x5cb544[_0x2d948a(0x205)](),
-        _0x366782 = document[_0x2d948a(0x1fa)]("li");
-      _0x366782[_0x2d948a(0x1df)][_0x2d948a(0x201)](
-        _0x2d948a(0x1fb),
-        _0x2d948a(0x20b),
-      ),
-        (_0x366782["innerHTML"] =
-          _0x2d948a(0x1e4) +
-          _0x5277c1[_0x2d948a(0x202)] +
-          _0x2d948a(0x20d) +
-          _0x5277c1[_0x2d948a(0x1cd)] +
-          _0x2d948a(0x1ee) +
-          _0x5277c1[_0x2d948a(0x204)] +
-          _0x2d948a(0x1ed) +
-          _0x5277c1["link"] +
-          "</a>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<button\x20class=\x22text-white\x20bg-black\x20h-7\x20flex\x20items-center\x20justify-center\x20w-auto\x20p-2\x20rounded-lg\x22>Visit</button>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<img\x20src=\x22projects/" +
-          _0x306078 +
-          _0x2d948a(0x209) +
-          _0x306078 +
-          _0x2d948a(0x1ec)),
-        _0xf675c2[_0x2d948a(0x1db)](_0x366782);
+  const response = await fetch("projects/");
+  const data = await response.json();
+  const div = document.querySelector(".glide__slides");
+
+  // Create array of promises for parallel execution
+  const promises = data
+    .filter((e) => !e.includes("."))
+    .map(async (e) => {
+      const fetchingfolder = await fetch(`projects/${e}/info.json`);
+      const fetcheddata = await fetchingfolder.json();
+
+      const li = document.createElement("li");
+      li.classList.add("glide__slide", "relative");
+
+      li.innerHTML = `
+                <div class="hidden absolute bottom-0 h-auto w-full bg-white opacity-80 rounded-lg z-10 items-center justify-center flex-col space-y-2 overflow-y-scroll p-2 border-t-2 border-gray-400">
+                    <div class="text-2xl font-bold">${fetcheddata.title}</div>
+                    <div class="text-sm">${fetcheddata.description}</div>
+                    <a class="text-white bg-black h-8 flex items-center justify-center p-2 rounded-lg w-[30%]" href="${fetcheddata.link}" target="_blank">Visit</a>
+                </div>
+                <img src="projects/${e}/image.png" alt="image" onerror="this.onerror=null; this.src='projects/${e}/image.avif';">
+            `;
+
+      div.appendChild(li);
     });
-  await Promise[_0x5c729e(0x1f1)](_0x2446a2),
-    new Glide(_0x5c729e(0x1fd), {
-      type: "carousel",
-      perView: 0x3,
-      autoplay: 0xbb8,
-      gap: 0xa,
-      breakpoints: { 0x400: { perView: 0x2 }, 0x300: { perView: 0x1 } },
-    })[_0x5c729e(0x1f4)]();
+
+  // Wait for all fetches to complete
+  await Promise.all(promises);
+
+  // Initialize Glide after all slides are loaded
+  new Glide(".glide-trending", {
+    type: "carousel",
+    perView: 3,
+    autoplay: 3000,
+    gap: 10,
+    breakpoints: {
+      1024: { perView: 2 },
+      768: { perView: 1 },
+    },
+  }).mount();
 }
-fetchprojectfolder(),
-  document["getElementById"](_0x4ed9dc(0x1e0))[_0x4ed9dc(0x1d5)](
-    _0x4ed9dc(0x1eb),
-    () => {
-      const _0x5f20e4 = _0x4ed9dc;
-      document[_0x5f20e4(0x1e9)]("aboutmescroll")["scrollIntoView"]({
-        behavior: _0x5f20e4(0x203),
-      });
-    },
-  ),
-  document[_0x4ed9dc(0x1e9)](_0x4ed9dc(0x20c))[_0x4ed9dc(0x1d5)](
-    "click",
-    () => {
-      const _0x2b3495 = _0x4ed9dc;
-      document[_0x2b3495(0x1e9)](_0x2b3495(0x1f3))[_0x2b3495(0x1e6)]({
-        behavior: _0x2b3495(0x203),
-      });
-    },
-  ),
-  document[_0x4ed9dc(0x1e9)](_0x4ed9dc(0x1e8))["addEventListener"](
-    "click",
-    () => {
-      const _0x34b6ff = _0x4ed9dc;
-      document["getElementById"](_0x34b6ff(0x20f))["scrollIntoView"]({
-        behavior: _0x34b6ff(0x203),
-      });
-    },
-  );
-const texts = ["Hi,\x20I\x20am\x20Priyanshu", _0x4ed9dc(0x1d3)];
-let textIndex = 0x0,
-  charIndex = 0x0,
-  typingSpeed = 0xc8,
-  erasingSpeed = 0x32,
-  delayBetweenTexts = 0x3e8;
+
+fetchprojectfolder();
+
+document.getElementById("about").addEventListener("click", () => {
+  document.getElementById("aboutmescroll").scrollIntoView({
+    behavior: "smooth",
+  });
+});
+document.getElementById("Projects").addEventListener("click", () => {
+  document.getElementById("projectscroll").scrollIntoView({
+    behavior: "smooth",
+  });
+});
+document.getElementById("contactme").addEventListener("click", () => {
+  document.getElementById("contactmescroll").scrollIntoView({
+    behavior: "smooth",
+  });
+});
+
+// for the typing effect
+const texts = ["Hi, I am Priyanshu", "Hi, I am a Developer."];
+let textIndex = 0;
+let charIndex = 0;
+let typingSpeed = 200;
+let erasingSpeed = 50;
+let delayBetweenTexts = 1000;
+
 function typeEffect() {
-  const _0x418d93 = _0x4ed9dc;
-  (document[_0x418d93(0x1e9)](_0x418d93(0x1f9))[_0x418d93(0x1f2)][
-    _0x418d93(0x1fc)
-  ] = _0x418d93(0x1f6)),
-    charIndex < texts[textIndex][_0x418d93(0x1e7)]
-      ? ((document[_0x418d93(0x1e9)](_0x418d93(0x1f9))[_0x418d93(0x1cc)] +=
-          texts[textIndex]["charAt"](charIndex)),
-        charIndex++,
-        setTimeout(typeEffect, typingSpeed))
-      : setTimeout(eraseEffect, delayBetweenTexts);
+  document.getElementById("typewriter").style.animation = "blink 0.5s infinite";
+  if (charIndex < texts[textIndex].length) {
+    document.getElementById("typewriter").innerHTML +=
+      texts[textIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(typeEffect, typingSpeed);
+  } else {
+    setTimeout(eraseEffect, delayBetweenTexts);
+  }
 }
+
 function eraseEffect() {
-  const _0x164605 = _0x4ed9dc;
-  (document[_0x164605(0x1e9)](_0x164605(0x1f9))[_0x164605(0x1f2)][
-    _0x164605(0x1fc)
-  ] = _0x164605(0x1f6)),
-    charIndex > 0x0
-      ? ((document[_0x164605(0x1e9)](_0x164605(0x1f9))[_0x164605(0x1cc)] =
-          texts[textIndex]["substring"](0x0, charIndex - 0x1)),
-        charIndex--,
-        setTimeout(eraseEffect, erasingSpeed))
-      : ((textIndex = (textIndex + 0x1) % texts["length"]),
-        setTimeout(typeEffect, typingSpeed));
+  document.getElementById("typewriter").style.animation = "blink 0.5s infinite";
+  if (charIndex > 0) {
+    document.getElementById("typewriter").innerHTML = texts[
+      textIndex
+    ].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(eraseEffect, erasingSpeed);
+  } else {
+    textIndex = (textIndex + 1) % texts.length;
+    setTimeout(typeEffect, typingSpeed);
+  }
 }
+
 typeEffect();
-const hamburgerMenu = document["getElementById"]("hamburgermenu"),
-  closeButton = document["getElementById"](_0x4ed9dc(0x1d8)),
-  hamburgerScroll = document[_0x4ed9dc(0x1e9)](_0x4ed9dc(0x1ef));
-hamburgerMenu[_0x4ed9dc(0x1d5)](_0x4ed9dc(0x1eb), () => {
-  const _0x4ba7d9 = _0x4ed9dc;
-  document["querySelector"](_0x4ba7d9(0x1f7))["classList"][_0x4ba7d9(0x200)](
-    _0x4ba7d9(0x20b),
-    _0x4ba7d9(0x206),
-  ),
-    hamburgerScroll["classList"][_0x4ba7d9(0x1fe)](
-      _0x4ba7d9(0x211),
-      _0x4ba7d9(0x1cf),
-      "pointer-events-none",
-    ),
-    hamburgerScroll[_0x4ba7d9(0x1df)][_0x4ba7d9(0x201)](
-      _0x4ba7d9(0x20e),
-      _0x4ba7d9(0x1f5),
-    );
-}),
-  closeButton["addEventListener"](_0x4ed9dc(0x1eb), () => {
-    const _0x1a096 = _0x4ed9dc;
-    hamburgerScroll[_0x1a096(0x1df)][_0x1a096(0x1fe)](
-      _0x1a096(0x20e),
-      _0x1a096(0x1f5),
-    ),
-      hamburgerScroll[_0x1a096(0x1df)][_0x1a096(0x201)](
-        _0x1a096(0x211),
-        "opacity-0",
-      ),
-      setTimeout(() => {
-        const _0x4814c1 = _0x1a096;
-        hamburgerScroll[_0x4814c1(0x1df)][_0x4814c1(0x201)](
-          "pointer-events-none",
-        ),
-          document["querySelector"](_0x4814c1(0x1f7))[_0x4814c1(0x1df)][
-            _0x4814c1(0x200)
-          ]("fixed", _0x4814c1(0x20b));
-      }, 0x1f4);
-  }),
-  document[_0x4ed9dc(0x1e9)]("aboutpart")[_0x4ed9dc(0x1d5)](
-    _0x4ed9dc(0x1eb),
-    () => {
-      const _0x105732 = _0x4ed9dc;
-      hamburgerScroll[_0x105732(0x1df)][_0x105732(0x1fe)](
-        "translate-x-0",
-        _0x105732(0x1f5),
-      ),
-        hamburgerScroll["classList"][_0x105732(0x201)](
-          _0x105732(0x211),
-          _0x105732(0x1cf),
-        ),
-        setTimeout(() => {
-          const _0x26f182 = _0x105732;
-          hamburgerScroll["classList"][_0x26f182(0x201)](_0x26f182(0x1e3)),
-            document["querySelector"]("body")[_0x26f182(0x1df)][
-              _0x26f182(0x200)
-            ](_0x26f182(0x206), _0x26f182(0x20b)),
-            document[_0x26f182(0x1e9)](_0x26f182(0x1e2))[_0x26f182(0x1e6)]({
-              behavior: _0x26f182(0x203),
-            });
-        }, 0x1f4);
-    },
-  ),
-  document[_0x4ed9dc(0x1e9)](_0x4ed9dc(0x1dc))[_0x4ed9dc(0x1d5)](
-    _0x4ed9dc(0x1eb),
-    () => {
-      const _0x35b2c4 = _0x4ed9dc;
-      hamburgerScroll["classList"][_0x35b2c4(0x1fe)](
-        _0x35b2c4(0x20e),
-        _0x35b2c4(0x1f5),
-      ),
-        hamburgerScroll[_0x35b2c4(0x1df)]["add"](
-          _0x35b2c4(0x211),
-          _0x35b2c4(0x1cf),
-        ),
-        setTimeout(() => {
-          const _0x5d87cc = _0x35b2c4;
-          hamburgerScroll[_0x5d87cc(0x1df)][_0x5d87cc(0x201)](_0x5d87cc(0x1e3)),
-            document[_0x5d87cc(0x1ce)](_0x5d87cc(0x1f7))[_0x5d87cc(0x1df)][
-              "replace"
-            ](_0x5d87cc(0x206), _0x5d87cc(0x20b)),
-            document[_0x5d87cc(0x1e9)]("projectscroll")["scrollIntoView"]({
-              behavior: _0x5d87cc(0x203),
-            });
-        }, 0x1f4);
-    },
+
+const hamburgerMenu = document.getElementById("hamburgermenu");
+const closeButton = document.getElementById("closebutton");
+const hamburgerScroll = document.getElementById("hamburgerScroll");
+
+hamburgerMenu.addEventListener("click", () => {
+  document.querySelector("body").classList.replace("relative", "fixed");
+
+  hamburgerScroll.classList.remove(
+    "translate-x-full",
+    "opacity-0",
+    "pointer-events-none",
   );
+  hamburgerScroll.classList.add("translate-x-0", "opacity-100");
+});
+
+closeButton.addEventListener("click", () => {
+  hamburgerScroll.classList.remove("translate-x-0", "opacity-100");
+  hamburgerScroll.classList.add("translate-x-full", "opacity-0");
+
+  setTimeout(() => {
+    hamburgerScroll.classList.add("pointer-events-none");
+    document.querySelector("body").classList.replace("fixed", "relative");
+  }, 500);
+});
+
+document.getElementById("aboutpart").addEventListener("click", () => {
+  hamburgerScroll.classList.remove("translate-x-0", "opacity-100");
+  hamburgerScroll.classList.add("translate-x-full", "opacity-0");
+
+  setTimeout(() => {
+    hamburgerScroll.classList.add("pointer-events-none");
+    document.querySelector("body").classList.replace("fixed", "relative");
+
+    document.getElementById("aboutmescroll").scrollIntoView({
+      behavior: "smooth",
+    });
+  }, 500);
+});
+
+document.getElementById("projectpart").addEventListener("click", () => {
+  hamburgerScroll.classList.remove("translate-x-0", "opacity-100");
+  hamburgerScroll.classList.add("translate-x-full", "opacity-0");
+
+  setTimeout(() => {
+    hamburgerScroll.classList.add("pointer-events-none");
+    document.querySelector("body").classList.replace("fixed", "relative");
+
+    document.getElementById("projectscroll").scrollIntoView({
+      behavior: "smooth",
+    });
+  }, 500);
+});
